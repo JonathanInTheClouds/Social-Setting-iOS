3278d258775ee6df17b868c0e921570270128938
Jonathan Dowdell <jdowde2@wgu.edu>
2020-10-24 23:29:47 -0500

    PostResponseModel +

---
+++ Social Setting/Models/PostModel.swift
@@ -0,0 +1,23 @@
+//
+//  PostModel.swift
+//  Social Setting
+//
+//  Created by Jonathan Dowdell on 10/23/20.
+//
+
+import Foundation
+
+struct PostResponseModel: Decodable {
+    let id: Int
+    let publicUserId: String
+    let postName: String
+    let url: String?
+    let description: String
+    let userName: String
+    let subSettingName: String
+    let duration: String
+    var upVote: Bool
+    var downVote: Bool
+    var voteCount: Int
+    var commentCount: Int
+}
