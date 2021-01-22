f4857e8342afa0094ba010480d1d3c3f387e0ef5
jonathandowdell <8318Pro2711>
2021-01-21 19:07:03 -0600

    DNP adjustment

--- Social Setting/View/Post/PostCreateSubscriptionListView.swift
+++ Social Setting/View/Post/PostCreateSubscriptionListView.swift
@@ -16,38 +16,30 @@ struct PostCreateSubscriptionListView: View {
     
     @State var tempSubSettingName = ""
     
+    fileprivate func listItem(_ targetName: String) -> Button<HStack<TupleView<(Text, Spacer, Image?)>>> {
+        return Button {
+            tempSubSettingName = targetName
+            UserDefaults.standard.set(tempSubSettingName, forKey: "SelectedSubSetting")
+        } label: {
+            HStack {
+                Text(targetName)
+                Spacer()
+                if let name = subSettingName, (targetName == name && tempSubSettingName.isEmpty) || (targetName == tempSubSettingName) {
+                    Image(systemName: "checkmark")
+                }
+            }
+        }
+    }
+    
     var body: some View {
         List {
             
-            if let currentUser = postCreateSubscriptionListViewModel.savedAuthentication {
-                Button {
-                    tempSubSettingName = currentUser.username
-                    UserDefaults.standard.set(tempSubSettingName, forKey: "SelectedSubSetting")
-                } label: {
-                    HStack {
-                        Text(currentUser.username)
-                        Spacer()
-                        if let name = subSettingName, (currentUser.username == name && tempSubSettingName.isEmpty) || (currentUser.username == tempSubSettingName) {
-                            Image(systemName: "checkmark")
-                        }
-                    }
-                }
+            if let targetName = postCreateSubscriptionListViewModel.savedAuthentication?.username {
+                listItem(targetName)
             }
             
             ForEach(postCreateSubscriptionListViewModel.subSettingList) { item in
-                Button {
-                    tempSubSettingName = item.name
-                    UserDefaults.standard.set(tempSubSettingName, forKey: "SelectedSubSetting")
-                } label: {
-                    HStack {
-                        Text(item.name)
-                        Spacer()
-                        if let name = subSettingName, (item.name == name && tempSubSettingName.isEmpty) || (item.name == tempSubSettingName) {
-                            Image(systemName: "checkmark")
-                        }
-                    }
-                }
-
+                listItem(item.name)
             }
         }
         .onDisappear {
