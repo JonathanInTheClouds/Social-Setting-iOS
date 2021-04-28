a9e5b574bf5429411d915418f282a3eecfd134e9
Jonathan M1 <jdowde2@wgu.edu>
2021-04-27 20:37:15 -0500

    Initial Commit

---
+++ Social Setting/Social_SettingApp.swift
@@ -0,0 +1,20 @@
+//
+//  Social_SettingApp.swift
+//  Social Setting
+//
+//  Created by Jonathan Dowdell on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Social_SettingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
